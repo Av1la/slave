57d5ef4a1133b2afc6cbf7d50b10f3ac6bb080ae
Diego Rodrigues <di3go.r0drigues@gmail.com>
2020-01-12 13:49:20 -0300

    reorganiza projeto. remove arquivos não mais utilizados.

--- makepackage.sh
+++ makepackage.sh
@@ -1,2 +1,4 @@
-source .virtualenv/bin/activate
+#!/bin/zsh
+
+source .venv/bin/activate
 python3 setup.py sdist bdist_wheel 
--- slave/tests/mpmailchecker.py
+++
@@ -1,9 +0,0 @@
-
-
-from app.workers.mpmailcheck import MPMailCheckWorker
-
-
-def check_single_mail():
-
-    worker = MPMailCheckWorker('felip3333333223232e@gmail.com')
-    worker.start()
